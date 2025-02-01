374013f4b8552373b15b97523298234dc258e626
unknown <ak06598909@gmail.com>
2025-02-02 00:33:48 +0500

    Translator

--- languagetranslation/app.py
+++ languagetranslation/app.py
@@ -1,1 +1,29 @@
-#Hello
+from flask import Flask, render_template, request
+from utils import Load_Model, Load_Tokenizer, Translate
+
+try:
+    print("Loading Artifacts ---> Start")
+    PATH = "translator"
+    tokenizer = Load_Tokenizer(tokenizer_path=PATH)
+    model = Load_Model(Model_Path=PATH)
+    print("Loading Artifacts ---> Done")
+
+except Exception as e:
+    raise e
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/main", methods=["GET", "POST"])
+def main():
+    translated_text = ""
+    if request.method == "POST":
+        sentence = request.form["sentence"]
+        translated_text = Translate(model=model, tokenizer=tokenizer, sentence=sentence)
+    return render_template("index.html", translated_text=translated_text)
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- languagetranslation/utils.py
+++ languagetranslation/utils.py
@@ -1,1 +1,25 @@
-#Hello
+from transformers import MarianTokenizer
+from transformers import MarianMTModel
+from pathlib import Path
+
+def Load_Model(Model_Path:Path) -> MarianMTModel:
+    try:
+        model=MarianMTModel.from_pretrained(Model_Path)
+        return model
+    except Exception as e:
+        print(f"Error Encountoured : {e}")
+
+def Load_Tokenizer(tokenizer_path:Path) -> MarianTokenizer:
+    try:
+        tokenizer=MarianTokenizer.from_pretrained(tokenizer_path)
+        return tokenizer
+    except Exception as e:
+        print(f"Error Encountoured : {e}")
+
+def Translate(model:MarianMTModel,tokenizer:MarianTokenizer,sentence:str) -> str:
+    try:
+        inputs = tokenizer(sentence, return_tensors="pt", padding=True, truncation=True)
+        translated = model.generate(**inputs)
+        return tokenizer.decode(translated[0], skip_special_tokens=True)
+    except Exception as e:
+        return e
